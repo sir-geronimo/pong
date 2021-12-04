2938c180099a2a8b56a648abb80877c064c574a5
Enger Jimenez <ejimenezr21@gmail.com>
2021-12-04 01:37:32 -0400

    Reformatted code

--- src/main.py
+++ src/main.py
@@ -29,6 +29,8 @@ obj_color = (175, 175, 175)
 # -------------
 pygame.display.set_caption("My awesome game")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
 # -------------
 
 
@@ -36,41 +38,41 @@ class Ball(Rect):
     x_speed = 10
     y_speed = 10
     size = (30, 30)
-    
+
     def __init__(self, position):
         super().__init__(position, self.size)
 
     def move(self):
         self.x += self.x_speed
         self.y += self.y_speed
-    
+
     def bounce(self):
-        if self.top <=0 or self.bottom >= SCREEN_HEIGHT:
+        if self.top <= 0 or self.bottom >= SCREEN_HEIGHT:
             self.y_speed *= -1
         if self.left <= 0 or self.right >= SCREEN_WIDTH:
             self.x_speed *= -1
-    
+
     def collide(self):
         if ball.colliderect(player) or ball.colliderect(opponent):
             self.x_speed *= -1
+
 
 class Player(Rect):
     pass
 
 
-
 # -------------
 # Game rectangles
 # -------------
-ball = Ball((SCREEN_WIDTH/2 - 15, SCREEN_HEIGHT/2 - 15))
+ball = Ball((SCREEN_WIDTH / 2 - 15, SCREEN_HEIGHT / 2 - 15))
 
 player = pygame.Rect(
-    (SCREEN_WIDTH - 20, SCREEN_HEIGHT/2 - 70),
+    (SCREEN_WIDTH - 20, SCREEN_HEIGHT / 2 - 70),
     player_size
 )
 
 opponent = pygame.Rect(
-    (10, SCREEN_HEIGHT/2 - 70),
+    (10, SCREEN_HEIGHT / 2 - 70),
     player_size
 )
 # -------------
@@ -103,9 +105,9 @@ while True:
     player.y += player_speed
     if player.top <= 0:
         player.top = 0
-    if player.bottom >=SCREEN_HEIGHT:
+    if player.bottom >= SCREEN_HEIGHT:
         player.bottom = SCREEN_HEIGHT
-        
+
     # Visuals
     screen.fill(bg_color)
 
@@ -116,10 +118,9 @@ while True:
     pygame.draw.aaline(
         screen,
         obj_color,
-        (SCREEN_WIDTH/2, 0),
-        (SCREEN_WIDTH/2, SCREEN_HEIGHT)
+        (SCREEN_WIDTH / 2, 0),
+        (SCREEN_WIDTH / 2, SCREEN_HEIGHT)
     )
-
 
     # Update screen
     pygame.display.update()
